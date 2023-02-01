19ce3d6ce56102fd961f504d9c06310f87fa02a2
soyombo <soyombo0@naver.com>
2023-02-01 14:55:43 +0300

    Initial Commit

---
+++ sonhirholtoi/sonhirholtoiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sonhirholtoiApp.swift
+//  sonhirholtoi
+//
+//  Created by Soyombo Mantaagiin on 1.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct sonhirholtoiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
